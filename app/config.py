6e670abf4cbafe43b3042e3b224a88229fd62cd3
Vincent Tetteh <vincent.tetteh@turntabl.io>
2022-12-03 23:25:01 +0000

    added ci file

--- app/config.py
+++ app/config.py
@@ -1,24 +1,25 @@
 from pydantic import BaseSettings
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 
 class Settings(BaseSettings):
-    database_host: str
-    database_name: str
-    database_username: str
-    database_password: str
-    database_port: str
-    algorithm: str
-    secret_key: str
-    access_token_expire_minutes: int
-    mail_username: str
-    mail_password: str
-    mail_from: str
-    mail_port: str
-    mail_server: str
-    mail_from_name: str
-
-    class Config:
-        env_file = ".env"
+    database_host: str = os.getenv('DATABASE_HOST')
+    database_name: str = os.getenv('DATABASE_NAME')
+    database_username: str = os.getenv('DATABASE_USERNAME')
+    database_password: str = os.getenv('DATABASE_PASSWORD')
+    database_port: str = os.getenv('DATABASE_PORT')
+    algorithm: str = os.getenv('ALGORITH')
+    secret_key: str = os.getenv('SECRET_KEY')
+    access_token_expire_minutes: int = os.getenv('ACCESS_TOKEN_EXPIRE_MINUTES')
+    mail_username: str = os.getenv('MAIL_USERNAME')
+    mail_password: str = os.getenv('MAIL_PASSWORD')
+    mail_from: str = os.getenv('MAIL_FROM')
+    mail_port: str = os.getenv('MAIL_PORT')
+    mail_server: str = os.getenv('MAIL_SERVER')
+    mail_from_name: str = os.getenv('MAIL_FROM_NAME')
 
 
 settings = Settings()
--- app/main.py
+++ app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from mangum import Mangum
+from fastapi.middleware.cors import CORSMiddleware
 from app import models,database
 from app.routers import auth, user, notes, password_reset
 
@@ -8,11 +10,23 @@ models.Base.metadata.create_all(bind=database.engine)
 
 app = FastAPI(title="Notebook App", version=0.1)
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(user.router)
 app.include_router(auth.router)
 app.include_router(notes.router)
 app.include_router(password_reset.router)
 
+
+handler = Mangum(app=app) 
 
 @app.get("/")
 def default():
